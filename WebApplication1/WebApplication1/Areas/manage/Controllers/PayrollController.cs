b1e559a704a1c9205a438c63c6dbcf225b4cab5a
XendamIbrahimli <xandamei-bp215@code.edu.az>
2024-12-06 15:56:00 +0400

    new one

---
+++ WebApplication1/WebApplication1/Areas/manage/Controllers/PayrollController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.manage.Controllers
+{
+    public class PayrollController(AppDbContext _context) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Payroll payroll)
+        {
+
+
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/AttendanceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AttendanceController(AppDbContext _context):Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Start(int Id)
+        {
+            var employee =await _context.Employees.FindAsync(Id);
+            if (employee == null)
+            {
+                BadRequest("This employee is not exist");
+            }
+            var Today = DateTime.Today;
+            
+            return View();
+        }
+
+    }
+}
