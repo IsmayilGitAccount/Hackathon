0b91c29c429d26acc3a05872820c6620dcee1e03
IsmayilGitAccount <ismayilni-bp217@code.edu.az>
2024-12-06 03:21:46 -0800

    sdffds

---
+++ WebApplication1/WebApplication1/Migrations/20241206093522_CreateVacationRequestsTables.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateVacationRequestsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "VacationRequests",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "VacationRequests");
+        }
+    }
+}
