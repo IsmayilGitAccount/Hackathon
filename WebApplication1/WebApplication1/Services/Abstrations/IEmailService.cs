4f2aa5b3fbbd18f63b079a30a06f41f85f2cfec9
Kanan-Q <kananag-bp215@code.edu.az>
2024-12-06 12:09:09 +0400

    Login

---
+++ WebApplication1/WebApplication1/Services/Abstrations/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Services.Abstrations
+{
+    public interface IEmailService
+    {
+        public void SendEmailConfirmation(string receiver, string name, string token);
+    }
+}
