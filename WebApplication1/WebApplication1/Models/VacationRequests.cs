391637ece6be0e2f276d939d1305305b90535f37
Kanan-Q <kananag-bp215@code.edu.az>
2024-12-06 15:09:39 +0400

    kanan

--- WebApplication1/WebApplication1/Models/VacationRequests.cs
+++ WebApplication1/WebApplication1/Models/VacationRequests.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string EmployeeId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string Status { get; set; }
         public string Reason { get; set; }
 
---
+++ WebApplication1/WebApplication1/ViewModels/Vacation/VacCreateVm.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.ViewModels.Vacation
+{
+    public class VacCreateVm
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+    }
+}
