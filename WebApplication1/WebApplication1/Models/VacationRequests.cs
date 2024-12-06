f4ee141a2fa9f1ddfc652b65296a281ec740436e
IsmayilGitAccount <ismayilni-bp217@code.edu.az>
2024-12-06 03:23:07 -0800

    delete cntrllr

--- WebApplication1/WebApplication1/Models/VacationRequests.cs
+++ WebApplication1/WebApplication1/Models/VacationRequests.cs
@@ -6,7 +6,7 @@
         public string EmployeeId { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string Status { get; set; }
+        public int Status { get; set; }
         public string Reason { get; set; }
 
         //relational
